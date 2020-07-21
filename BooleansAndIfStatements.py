f0f17942d07d573e241f8782e40ad89d1958cbb1
Kriti Bhardwaj <kritibhardwaj@icloud.com>
2020-07-21 16:53:00 +1000

    Update BooleansAndIfStatements.py
    Solution to Q3 and Q4

--- BooleansAndIfStatements.py
+++ BooleansAndIfStatements.py
@@ -25,5 +25,33 @@ elif moths_in_house and not mitch_is_house:
 elif not moths_in_house and mitch_is_house:
     print("Climb on Mitch.")
 
+ # Q3) Write a program that implements the algorithm for Red Light Cameras
+light_colour = "Green"
+car_detected = False
+
+if light_colour == "Red" and car_detected:
+    print("Flash!")
+else:
+    print("Do Nothing.")
+# elif light_colour == "Red" and car_detected:
+#     print("Flash!")
+# elif light_colour == "Green" and not car_detected:
+#     print("Do Nothing.")
+# elif light_colour == "Green" and car_detected:
+#     print("Do Nothing.")
+# elif light_colour == "Amber" and not car_detected:
+#     print("Do Nothing.")
+# elif light_colour == "Amber" and car_detected:
+#     print("Do Nothing.")
+
+
+# Q4) Write a program that asks the user for their height, and determine whether or not they are tall enough to
+# ride the rollercoaster, which has a height requirement of 120cms
+height = input("Enter your height: ")
+if int(height) >= 120:
+    print("Hop On!")
+else:
+    print("Sorry, not today :(")
+
 
 
