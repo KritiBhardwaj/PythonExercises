b89470cdc476e6da1cef4b122ccf88e2369e46c2
Kriti Bhardwaj <kritibhardwaj@icloud.com>
2020-07-21 13:49:03 +1000

    Create VariableAndUserInput.py
    Answer codes for question 1, 2 and 3 for exercises for Variable and User Input

---
+++ VariableAndUserInput.py
@@ -0,0 +1,20 @@
+# program that takes two numbers from the user, and outputs their sum
+num1A = input("Enter the first number: ")
+num1B = input("Enter the second number: ")
+sum = int(num1A) + int(num1B)
+print(sum)
+
+# program that takes two numbers from the user, and outputs the equation representing the
+# multiplication of the two numbers
+num2A = input("Enter the first multiple: ")
+num2B = input("Enter the second multiple: ")
+product = int(num2A) * int(num2B)
+print(f"{num2A} * {num2B} = {product}")
+
+# Write a program that takes a distance in kilometers from the user, and output the distance in meters and
+# centimeters
+distance = input("Enter distance in kilometers: ")
+outputInMeters = int(distance) * 1000
+outputinCentimeters = int(distance) * 100000
+print (f"{distance}km = {outputInMeters}m")
+print (f"{distance}km = {outputinCentimeters}cm")
