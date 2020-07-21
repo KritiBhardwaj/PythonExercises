de1792c39f600ceedd4979cd3e1e2dce1d810125
Kriti Bhardwaj <kritibhardwaj@icloud.com>
2020-07-21 14:15:03 +1000

    Create BooleansAndIfStatements.py
    Q1 solution attempt 1

---
+++ BooleansAndIfStatements.py
@@ -0,0 +1,6 @@
+# Q1) Write a program that determines whether or not it is time for Roary the cat to catch moths
+moths_in_house = input("Enter True or False: ")
+if moths_in_house:
+    print("Get the moths!")
+else:
+    print("No threats detected.")
