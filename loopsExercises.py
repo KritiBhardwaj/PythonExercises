039240e4e92f72da5fcf19db451a0c009dd390a1
Kriti Bhardwaj <kritibhardwaj@icloud.com>
2020-07-25 15:17:02 +1000

    Delete loopsExercises.py
    redundant file

--- loopsExercises.py
+++
@@ -1,7 +0,0 @@
-# Q1) Continuously ask the user to enter a number until they provide a blank input. Output the sum of all the
-# numbers
-
-number = 0 
-while number != "":
-    number = input("Enter a number: ")
-    number = int(number) + number
