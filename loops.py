68e1d818c1e03a1bf7267b2abb8cfb5b9181e0e3
Kriti Bhardwaj <kritibhardwaj@icloud.com>
2020-07-25 15:17:12 +1000

    Update loops.py
    update to q1

--- loops.py
+++ loops.py
@@ -1,12 +1,12 @@
-# # Q1) Continuously ask the user to enter a number until they provide a blank input. Output the sum of all the
-# # numbers
-# # number = 0
-# # sum = 0
-# # while number != '':
-# #     number = input("Enter a number: ")
-# #     if number:
-# #         sum = sum + int(number)
-# #         print(sum)
+# Q1) Continuously ask the user to enter a number until they provide a blank input. Output the sum of all the
+# numbers
+# number = 0
+# sum = 0
+# while number != '':
+#     number = input("Enter a number: ")
+#     if number:
+#         sum = sum + int(number)
+#         print(sum)
 
 
 # sum = 0
@@ -16,6 +16,13 @@
 #     sum = sum + int(number)
 #     print(f"The sum is {sum}")
 
+
+number = 0
+sum = 0
+while number != "":
+    sum = sum + int(number)
+    number = input("Enter a number:")
+print(sum)
 
 
 # Q2) Use a for loop to format and print the following list:
